320c2f9afb0dd9118debb4ee985b8b911277266f
Антон <antonbliznuk71@gmail.com>
2026-02-03 13:13:07 +0000

    Installed pytest and set it up

---
+++ backend/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from rest_framework.test import APIClient
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+@pytest.fixture
+def user(db):
+    return User.objects.create_user(
+        email="user@test.com",
+        password="password123"
+    )
