495b2eabe1ffb845b1c79e3c2cff9c4505d45ebf
Антон <antonbliznuk71@gmail.com>
2026-02-04 15:05:30 +0000

    CORS error fix

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -5,6 +5,12 @@ from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
 
+from corsheaders.defaults import default_headers
+
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    "authorization",
+]
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,10 +32,19 @@ USE_X_FORWARDED_HOST = True
 
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(",")
+
+
 CSRF_TRUSTED_ORIGINS = [
-    "https://alert-ambition-dev.up.railway.app",
+    "https://volunteer-site-placeholder-dev.up.railway.app"
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:8000",
+    "https://volunteer-site-placeholder-dev.up.railway.app",
+]
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
@@ -65,13 +80,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:8000",
-    "https://volunteer-site-placeholder-dev.up.railway.app",
-]
-CORS_ALLOW_CREDENTIALS = True
+
 
 if DEBUG:
     INSTALLED_APPS += ["debug_toolbar"]
