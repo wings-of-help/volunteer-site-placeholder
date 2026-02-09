1100977577516c2ed2b8820e451544be42f72c5c
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2026-02-09 21:46:21 +0200

    feat: implement request deletion with confirm modal and optimistic UI update

--- src/api/help.api.ts
+++ src/api/help.api.ts
@@ -3,11 +3,9 @@ import { authFetch } from './authFetch';
 import type { HelpRequest } from './types/help';
 import type { Paginated } from './types/common';
 
-export const getMyRequests = async (
-  userId: number
-): Promise<HelpRequest[]> => {
+export const getMyRequests = async (userId: number): Promise<HelpRequest[]> => {
   const res = await authFetch(
-    `${BASE_URL}/help/?kind=request&creator=${userId}&ordering=-created_at`
+    `${BASE_URL}/help/?kind=request&creator=${userId}&ordering=-created_at`,
   );
 
   if (!res.ok) {
@@ -20,7 +18,7 @@ export const getMyRequests = async (
 
 export const getMyOffers = async (userId: number) => {
   const res = await authFetch(
-    `${BASE_URL}/help/?kind=offer&creator=${userId}&ordering=-created_at`
+    `${BASE_URL}/help/?kind=offer&creator=${userId}&ordering=-created_at`,
   );
 
   if (!res.ok) {
@@ -31,9 +29,11 @@ export const getMyOffers = async (userId: number) => {
   return data.results;
 };
 
-export const getMyResponses = async (userId: number): Promise<HelpRequest[]> => {
+export const getMyResponses = async (
+  userId: number,
+): Promise<HelpRequest[]> => {
   const res = await authFetch(
-    `${BASE_URL}/help/?counterpart=${userId}&ordering=-created_at`
+    `${BASE_URL}/help/?counterpart=${userId}&ordering=-created_at`,
   );
 
   if (!res.ok) {
@@ -44,3 +44,16 @@ export const getMyResponses = async (userId: number): Promise<HelpRequest[]> => 
   return data.results;
 };
 
+// Make it done in_progress--->done!
+// поки не працює!
+export const completeHelpRequest = (id: number) => {
+  return authFetch(`${BASE_URL}/help/${id}/complete/`, {
+    method: 'POST',
+  });
+};
+
+export const deleteHelpRequest = (id: number) => {
+  return authFetch(`${BASE_URL}/help/${id}/`, {
+    method: 'DELETE',
+  });
+};
