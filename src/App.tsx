3d22d8c3592af70bcb2eb498cab0974e9764080c
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2026-02-12 19:31:54 +0200

    feat: add i18n support for user roles in profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import { useAuth } from './context/AuthContext';
+// import { useAuth } from './context/AuthContext';
 import './index.scss';
 
 export default function App() {
   const location = useLocation();
-  const { user } = useAuth();
+  // const { user } = useAuth();
 
-  const isHomePage = location.pathname === '/';
+  // const isHomePage = location.pathname === '/';
   const isProfilePage = location.pathname.startsWith('/profile');
 
   // фон тільки для home + не логінений
-  const withHomeBackground = isHomePage && !user;
+  // const withHomeBackground = isHomePage && !user;
 
   return (
-    <div className={`home-layout ${withHomeBackground ? 'home-layout--bg' : ''}`}>
+    // <div className={`home-layout ${withHomeBackground ? 'home-layout--bg' : ''}`}>
+    <div className='home-layout home-layout--bg'>
       <Header />
       <Outlet />
       {!isProfilePage && <Footer />}
