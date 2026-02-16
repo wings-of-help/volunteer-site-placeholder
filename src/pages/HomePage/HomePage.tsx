4b8880e6f820d4daf981817fc16b2171012edf2e
Roman Rusin <rgromkorusin12@gmail.com>
2026-02-17 01:03:26 +0200

    home

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,18 @@ import { useTranslation } from "react-i18next";
 import "./HomePage.scss"
 import { Link } from "react-router-dom";
 import ActiveGroup from "../../components/ActiveGroup/ActiveGroup";
+import photo1 from "../../assets/post-1.png"
+import photo2 from "../../assets/post-2.png"
+import photo3 from "../../assets/post-3.png"
+import photo4 from "../../assets/post-4.png"
+import bigpost1 from "../../assets/Illustration-1.png"
+import bigpost2 from "../../assets/Illustration-2.png"
+
+import icon1 from "../../assets/Home-page-icon1.svg"
+import icon2 from "../../assets/Home-page-icon2.svg"
+import icon3 from "../../assets/Home-page-icon3.svg"
+import icon4 from "../../assets/Home-page-icon4.svg"
+import icon5 from "../../assets/Home-page-icon5.svg"
   
 export default function HomePage() {
   const { t } = useTranslation();
@@ -36,35 +48,27 @@ export default function HomePage() {
         <div className="home-instructions__blocks">
 
           <div className="home-instructions__blocks__block">
+            <img className="home-instructions__blocks__block__p" src={photo1} />
+            <div className="home-instructions__blocks__block__num">01</div>
             <div className="home-instructions__blocks__block__title">{t("instructions-1st-block-title")}</div>
-
-            <div className="home-instructions__blocks__block__p">
-              {t("instructions-1st-block-p")}
-            </div>
           </div>
 
           <div className="home-instructions__blocks__block">
+            <img className="home-instructions__blocks__block__p" src={photo2} />
+            <div className="home-instructions__blocks__block__num">02</div>
             <div className="home-instructions__blocks__block__title">{t("instructions-2nd-block-title")}</div>
-
-            <div className="home-instructions__blocks__block__p">
-              {t("instructions-2nd-block-p")}
-            </div>
           </div>
 
           <div className="home-instructions__blocks__block">
+            <img className="home-instructions__blocks__block__p" src={photo3} />
+            <div className="home-instructions__blocks__block__num">03</div>
             <div className="home-instructions__blocks__block__title">{t("instructions-3rd-block-title")}</div>
-
-            <div className="home-instructions__blocks__block__p">
-              {t("instructions-3rd-block-p")}
-            </div>
           </div>
 
           <div className="home-instructions__blocks__block">
+            <img className="home-instructions__blocks__block__p" src={photo4} />
+            <div className="home-instructions__blocks__block__num">04</div>
             <div className="home-instructions__blocks__block__title">{t("instructions-4th-block-title")}</div>
-
-            <div className="home-instructions__blocks__block__p">
-              {t("instructions-4th-block-p")}
-            </div>
           </div>
         </div>
       </div>
@@ -75,34 +79,59 @@ export default function HomePage() {
         <div className="home-description__blocks">
 
           <div className="home-description__blocks__block">
-            <div className="home-description__blocks__block__title">{t("description-1st-block-title")}</div>
+            <img className="home-instructions__blocks__block__photo" src={bigpost1} />
+            
+            <div className="home-description__blocks__block__wrapper">
+              <div className="home-description__blocks__block__title">{t("description-1st-block-title")}</div>
 
-            <div className="home-description__blocks__block__paragraphs">
-              <p className="home-description__blocks__block__paragraphs__p">
-                {t("description-1st-block-1st-p")}
-              </p>
-              <p className="home-description__blocks__block__paragraphs__p">
-                {t("description-1st-block-2nd-p")}
-              </p>
-              <p className="home-description__blocks__block__paragraphs__p">
-                {t("description-1st-block-3rd-p")}
-              </p>
+              <div className="home-description__blocks__block__paragraphs">
+                <div className="home-description__blocks__block__paragraphs__wrapper">
+                  <img src={icon1} alt="icon1" className="icon" />
+
+                  <p className="home-description__blocks__block__paragraphs__p">
+                    {t("description-1st-block-1st-p")}
+                  </p>
+                </div>
+                <div className="home-description__blocks__block__paragraphs__wrapper">
+                  <img src={icon2} alt="icon1" className="icon"/>
+
+                   <p className="home-description__blocks__block__paragraphs__p">
+                    {t("description-1st-block-2nd-p")}
+                  </p>
+                </div>
+                <div className="home-description__blocks__block__paragraphs__wrapper">
+                  <img src={icon3} alt="icon1" className="icon" />
+
+                  <p className="home-description__blocks__block__paragraphs__p">
+                    {t("description-1st-block-3rd-p")}
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
 
           <div className="home-description__blocks__block">
-            <div className="home-description__blocks__block__title">{t("description-2nd-block-title")}</div>
+            <img className="home-instructions__blocks__block__photo" src={bigpost2} />
+            
+            <div className="home-description__blocks__block__wrapper">
+              <div className="home-description__blocks__block__title">{t("description-2nd-block-title")}</div>
 
-            <div className="home-description__blocks__block__paragraphs">
-              <p className="home-description__blocks__block__paragraphs__p">
-                {t("description-2nd-block-1st-p")}
-              </p>
-              <p className="home-description__blocks__block__paragraphs__p">
-                {t("description-2nd-block-2nd-p")}
-              </p>
-              <p className="home-description__blocks__block__paragraphs__p">
-                {t("description-2nd-block-3rd-p")}
-              </p>
+              <div className="home-description__blocks__block__paragraphs">
+                <div className="home-description__blocks__block__paragraphs__wrapper">
+                  <img src={icon4} alt="icon1" className="icon" />
+
+                  <p className="home-description__blocks__block__paragraphs__p">
+                    {t("description-2nd-block-1st-p")}
+                  </p>
+                </div>
+                <div className="home-description__blocks__block__paragraphs__wrapper">
+                  <img src={icon5} alt="icon1" className="icon"/>
+
+                   <p className="home-description__blocks__block__paragraphs__p">
+                    {t("description-2nd-block-2nd-p")}
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
