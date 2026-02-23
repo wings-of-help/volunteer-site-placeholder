90ad1058c14f39289d9f1f7a9eac4b37d95d28e8
Антон <antonbliznuk71@gmail.com>
2026-02-23 20:29:06 +0000

    filters fix

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -191,12 +191,6 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": True,
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 
 ADMIN_SECRET_CODE = os.environ.get("ADMIN_SECRET_CODE")
 
