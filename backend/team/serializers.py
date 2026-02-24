7f3f9a602f449ef2d08434a3d219472cc601f0d6
Антон <antonbliznuk71@gmail.com>
2026-02-24 19:53:56 +0000

    fixes

--- backend/team/serializers.py
+++ backend/team/serializers.py
@@ -15,6 +15,7 @@ class SocialLinkSerializer(serializers.ModelSerializer):
 
 class TeammateSerializer(serializers.ModelSerializer):
     social_links = SocialLinkSerializer(many=True)
+    photo = serializers.SerializerMethodField()
 
     class Meta:
         model = Teammate
