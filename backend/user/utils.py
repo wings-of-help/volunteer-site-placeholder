19164e4bb23b6b336e5e0fca241b13f593442b7d
Антон <antonbliznuk71@gmail.com>
2026-02-28 19:03:32 +0000

    fixes

--- backend/user/utils.py
+++ backend/user/utils.py
@@ -11,12 +11,12 @@ def send_email(to_email, subject, html_content):
     response = requests.post(
         "https://api.resend.com/emails",
         headers={
-            "Authorization": f"Bearer {os.getenv("RESEND_API_KEY")}",
+            "Authorization": f"Bearer {os.getenv('RESEND_API_KEY')}",
             "Content-Type": "application/json",
         },
         json={
             "from": os.getenv("DEFAULT_FROM_EMAIL"),
-            "to": [to_email],
+            "to": to_email,
             "subject": subject,
             "html": html_content,
         },
